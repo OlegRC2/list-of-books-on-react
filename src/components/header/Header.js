9a8d73d82859ab5affe1cafb51b0d0b8876c6fdd
Oleg <p.oleg2@mail.ru>
2021-10-01 01:35:47 +0500

    Added link to git

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,10 +3,10 @@ import './header.css';
 function Header() {
     return <nav className="green darken-1">
                 <div className="nav-wrapper">
-                    <a href="#" className="brand-logo">Books</a>
+                    <a href="!#" className="brand-logo">Books</a>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
                         <li>
-                            <a href="#">Repo</a>
+                            <a href="https://github.com/OlegRC2/list-of-books-on-react">Repo</a>
                         </li>
                     </ul>
                 </div>
